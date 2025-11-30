b916adbd55fba2e668b94eb962baf03cd81c9ee9
Pedro Paulo <pedro.ppsilva07@gmail.com>
2025-11-30 01:26:35 +0000

    Try fixing this error: `Runtime Error: Error: ShoppingCart is not define

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,6 +9,7 @@ import {
   Package,
   Clock,
   AlertTriangle,
+  ShoppingCart,
 } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
