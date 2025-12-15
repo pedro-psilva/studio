1429142da38642fec8e728708f3eb6ebd79d9988
Pedro Paulo <pedro.ppsilva07@gmail.com>
2025-12-15 11:22:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "it-lab-solution-2-708503-5453c",
+  "appId": "1:290067105326:web:66bc16cf13cbbdc4d041bf",
+  "apiKey": "AIzaSyDeOZb2amwldaX5-t2WKETN42oc9qR6Doo",
+  "authDomain": "it-lab-solution-2-708503-5453c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "290067105326"
+};
